968c0f7a50abcddf5c957c91206dab9602304212
sigmaidetest <cehib@balanc3r.com>
2018-08-28 17:35:04 +0530

    Reading from s3

--- XMLTest/lambda.js
+++ XMLTest/lambda.js
@@ -3,56 +3,28 @@ const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
 
-    let xmlString = '<xml xmlns="a"><child>test</child></xml>';
+    let companyName = event.queryStringParameters['comp'];
+    let fileName = `${companyName}.xml`;
 
     s3.getObject({
         'Bucket': "sample.quotes",
-        'Key': "test.xml"
+        'Key': fileName
     }).promise()
         .then(data => {
-            console.log(data);           // successful response
-            /*
-            data = {
-                AcceptRanges: "bytes", 
-                ContentLength: 3191, 
-                ContentType: "image/jpeg", 
-                ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
-                LastModified: <Date Representation>, 
-                Metadata: {...}, 
-                TagCount: 2, 
-                VersionId: "null"
-            }
-            */
+            let body = data.Body;
+            let xmlString = body.toString('utf8');
+            callback(null, {
+                "isBase64Encoded": true,
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/xml"
+                },
+                "body": xmlString
+            });
         })
         .catch(err => {
             console.log(err, err.stack); // an error occurred
+            callback(err);
         });
-
-
-    callback(null, {
-        "isBase64Encoded": true,
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/xml"
-        },
-        "body": xmlString
-    });
 }
-
-// {
-//   "isBase64Encoded": true,
-//   "statusCode": 200,
-//   "headers": {
-//     "headerName": "headerValue"
-//   },
-//   "body": "..."
-// }Access-Control-Allow-Origin: * |
-//   allowed domains for CORS access
-// Access-Control-Allow-Methods: * |
-//   allowed HTTP methods for CORS
-// Access-Control-Allow-Headers: * |
-//   request headers client can send
-// Access-Control-Expose-Headers: 
-//   *,x-amzn-remapped-authorization |
-//   response headers client can read
