9bc24e69de3bd901a8b7a95acfab52316ff40788
sigmaidetest <cehib@balanc3r.com>
2018-08-28 17:17:49 +0530

    added s3 bucket

--- XMLTest/lambda.js
+++ XMLTest/lambda.js
@@ -1,8 +1,33 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
 
     let xmlString = '<xml xmlns="a"><child>test</child></xml>';
+
+    s3.getObject({
+        'Bucket': "sample.quotes",
+        'Key': "test.xml"
+    }).promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                AcceptRanges: "bytes", 
+                ContentLength: 3191, 
+                ContentType: "image/jpeg", 
+                ETag: "\\"6805f2cfc46c0f04559748bb039d69ae\\"", 
+                LastModified: <Date Representation>, 
+                Metadata: {...}, 
+                TagCount: 2, 
+                VersionId: "null"
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
 
     callback(null, {
         "isBase64Encoded": true,
